29741c6167e2b2f9d0870b68a78d45129d9c63c5
Amanda <31946428+ManderWanders@users.noreply.github.com>
2018-11-16 11:29:41 -0500

    fixed comments in js file

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-// Function generates random number for "Ranked" document.write, showing Rick's place in the game "Red Dead Redemption 2".
+// Function generates random number for "Ranked"
 
 function randomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
@@ -8,6 +8,7 @@ var counter = 0;
 
 while ( counter < 1 ) {
   var randNum = randomNumber(6);
+  //document.write, showing Rick's place in the game "Red Dead Redemption 2"
   document.write( 'Ranked # ' + randNum + ' of a lot in Red Dead Redemption 2.' );
   counter += 1;
 }
